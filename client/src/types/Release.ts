90144d78261b8fd38a84bb6fb946d34822b25eef
Denis Kolokolov <dennis.kolokolov@gmail.com>
2025-02-12 19:33:06 +0100

    fix(KXP-24): correct Release type structure

--- client/src/types/Release.ts
+++ client/src/types/Release.ts
@@ -7,19 +7,17 @@ import { Community } from './Community';
 import { Image } from './Image';
 
 export interface Release {
-  release: {
-    images: Image[];
-    title: string;
-    artists: Artist[];
-    formats: Format[];
-    year: number;
-    genres: string[];
-    styles: string[];
-    videos: Video[];
-    released_formatted: string;
-    labels: Label[];
-    country: string;
-    tracklist: Track[];
-    community: Community;
-  };
+  images: Image[];
+  title: string;
+  artists: Artist[];
+  formats: Format[];
+  year: number;
+  genres: string[];
+  styles: string[];
+  videos: Video[];
+  released_formatted: string;
+  labels: Label[];
+  country: string;
+  tracklist: Track[];
+  community: Community;
 }
